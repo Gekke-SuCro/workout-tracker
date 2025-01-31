5d7b0eb990d01c490b6e755c2d0634f1a0fa7cf3
Gekke-SuCro <jayden.roeper@student.hu.nl>
2025-01-31 11:04:13 +0100

    feat: Implemented path protection.
    Depending if authorized or not, certain paths will redirect the user.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,42 @@
-import { StrictMode } from "react";
+import { StrictMode, useMemo } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import "./index.css";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 
+// eslint-disable-next-line react-refresh/only-export-components, react/prop-types
+const AuthGuard = ({ children }) => {
+  const isAuthenticated = useMemo(() => !!localStorage.getItem("token"), []);
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
+// eslint-disable-next-line react-refresh/only-export-components, react/prop-types
+const LoginGuard = ({ children }) => {
+  const isAuthenticated = useMemo(() => !!localStorage.getItem("token"), []);
+  return isAuthenticated ? <Navigate to="/" /> : children;
+};
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: (
+      <AuthGuard>
+        <HomePage />
+      </AuthGuard>
+    ),
   },
   {
     path: "/login",
-    element: <LoginPage />,
+    element: (
+      <LoginGuard>
+        <LoginPage />
+      </LoginGuard>
+    ),
   },
 ]);
 
