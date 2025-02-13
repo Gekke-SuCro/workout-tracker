00a49033683e6548ccb51c67984854405bed413c
Gekke-SuCro <jayden.roeper@student.hu.nl>
2025-02-13 15:10:52 +0100

    refactor: use java streams for mapping between dto and class

--- backend/src/main/java/com/jaydenroeper/workouttracker/backend/workout/mapper/WorkoutExerciseMapper.java
+++ backend/src/main/java/com/jaydenroeper/workouttracker/backend/workout/mapper/WorkoutExerciseMapper.java
@@ -1,32 +1,18 @@
 package com.jaydenroeper.workouttracker.backend.workout.mapper;
 
-import com.jaydenroeper.workouttracker.backend.workout.application.dto.ExerciseSetResponseDto;
 import com.jaydenroeper.workouttracker.backend.workout.application.dto.WorkoutExerciseResponseDto;
-import com.jaydenroeper.workouttracker.backend.workout.application.exception.ExerciseNotFoundException;
-import com.jaydenroeper.workouttracker.backend.workout.data.ExerciseRepository;
 import com.jaydenroeper.workouttracker.backend.workout.domain.Exercise;
-import com.jaydenroeper.workouttracker.backend.workout.domain.ExerciseSet;
 import com.jaydenroeper.workouttracker.backend.workout.domain.WorkoutExercise;
 import com.jaydenroeper.workouttracker.backend.workout.presentation.dto.ExerciseSetRequestDto;
 import com.jaydenroeper.workouttracker.backend.workout.presentation.dto.WorkoutExerciseRequestDto;
-import org.hibernate.jdbc.Work;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 public class WorkoutExerciseMapper {
 
     public static WorkoutExerciseResponseDto toDto(WorkoutExercise workoutExercise) {
-        List<ExerciseSetResponseDto> sets = new ArrayList<>();
-        for (ExerciseSet es : workoutExercise.getSets()) {
-            sets.add(ExerciseSetMapper.toDto(es));
-        }
-
         return new WorkoutExerciseResponseDto(
                 workoutExercise.getId(),
                 workoutExercise.getExercise().getName(),
-                sets
+                workoutExercise.getSets().stream().map(ExerciseSetMapper::toDto).toList()
         );
     }
 
--- backend/src/main/java/com/jaydenroeper/workouttracker/backend/workout/mapper/WorkoutMapper.java
+++ backend/src/main/java/com/jaydenroeper/workouttracker/backend/workout/mapper/WorkoutMapper.java
@@ -1,41 +1,30 @@
 package com.jaydenroeper.workouttracker.backend.workout.mapper;
 
-import com.jaydenroeper.workouttracker.backend.workout.application.dto.WorkoutExerciseResponseDto;
 import com.jaydenroeper.workouttracker.backend.workout.application.dto.WorkoutResponseDto;
-import com.jaydenroeper.workouttracker.backend.workout.data.ExerciseRepository;
 import com.jaydenroeper.workouttracker.backend.workout.domain.Exercise;
 import com.jaydenroeper.workouttracker.backend.workout.domain.Workout;
-import com.jaydenroeper.workouttracker.backend.workout.domain.WorkoutExercise;
 import com.jaydenroeper.workouttracker.backend.workout.presentation.dto.WorkoutExerciseRequestDto;
 import com.jaydenroeper.workouttracker.backend.workout.presentation.dto.WorkoutRequestDto;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class WorkoutMapper {
 
     public static WorkoutResponseDto toDto(Workout workout) {
-        List<WorkoutExerciseResponseDto> exercises = new ArrayList<>();
-        for (WorkoutExercise ex : workout.getExercises()) {
-            exercises.add(WorkoutExerciseMapper.toDto(ex));
-        }
-
         return new WorkoutResponseDto(
                 workout.getId(),
                 workout.getName(),
                 workout.getDate(),
-                exercises
+                workout.getExercises().stream().map(WorkoutExerciseMapper::toDto).toList()
         );
     }
 
     public static Workout toWorkout(WorkoutRequestDto workoutRequestDto, List<Exercise> exercises) {
         Workout workout = new Workout(workoutRequestDto.name(), workoutRequestDto.date());
-
         for (WorkoutExerciseRequestDto ex : workoutRequestDto.exercises()) {
             Exercise matchedExercise = findExerciseByName(ex.name(), exercises);
             workout.addExercise(WorkoutExerciseMapper.toWorkoutExercise(ex, matchedExercise));
         }
-
         return workout;
     }
 
